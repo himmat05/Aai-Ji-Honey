5a2ffc3b155cb9e9927b59a4cb53227251346cb7
Himmat Sirwee <himmatsirwee05@gmail.com>
2025-07-31 09:58:05 +0530

    updated mongo connect

--- backend/server.js
+++ backend/server.js
@@ -93,12 +93,16 @@ app.post("/register-owner", async (req, res) => {
   }
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log("MongoDB Atlas Connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose.connect(process.env.MONGO_URI, {
+//   // useNewUrlParser: true,
+//   // useUnifiedTopology: true
+//   .then(() => console.log("MongoDB Atlas Connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+// })
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 // Multer setup
 const storage = multer.diskStorage({
