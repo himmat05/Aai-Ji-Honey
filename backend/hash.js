fcf44e05590462ff2f375498f6a45e69967f41fa
Himmat Sirwee <himmatsirwee05@gmail.com>
2025-07-31 07:54:13 +0530

    Remove hash.js from backend

--- backend/hash.js
+++
@@ -1,9 +0,0 @@
-// hash.js
-const bcrypt = require("bcryptjs");
-
-const password = "0477400169"; // Replace with your desired password
-
-bcrypt.hash(password, 10, (err, hash) => {
-  if (err) throw err;
-  console.log("Hashed password:", hash);
-});
