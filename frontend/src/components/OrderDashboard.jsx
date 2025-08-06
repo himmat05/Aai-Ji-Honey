adcae1a7c0e028fc11022fe45b8b02f0da3b9cf7
Himmat Sirwee <himmatsirwee05@gmail.com>
2025-08-06 19:03:20 +0530

    print edit

--- frontend/src/components/OrderDashboard.jsx
+++ frontend/src/components/OrderDashboard.jsx
@@ -59,138 +59,97 @@ const OrderDashboard = () => {
 
         const printWindow = window.open('', '', 'width=800,height=600');
 
-        printWindow.document.write(`
-    <html>
-      <head>
-        <title>Invoice</title>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-            padding: 20px;
-          }
-          h2 {
-            margin-bottom: 10px;
-          }
-          table {
-            width: 100%;
-            border-collapse: collapse;
-          }
-          table, th, td {
-            border: 1px solid black;
-          }
-          th, td {
-            padding: 8px;
-            text-align: left;
-          }
-        </style>
-      </head>
-      <body>
-        ${content.innerHTML}
-        {/* <div>
-                    <div className='bg-green-300 border-2 border-black'>
-                        <div className='mx-auto text-center mt-2'>
-                            <h1>Jai Shri Aai Mata Namo Namah</h1>
-                            <h1 className='underline'>Tax Invoice</h1>
-                        </div>
-                        <div className='flex items-center justify-around gap-10 mt-2'>
-                            <div className='ml-5'>
-                                <h1>GSTIN:08HIAPS1709H1Z5</h1>
-                                <h1>Email: aaijihoney24@gmail.com</h1>
-                            </div>
-                            <div>
-                                <h1>Contact : +91 9610047740</h1>
-                                <h1 className='ml-17'>+91 9887918251</h1>
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-2 mt-0.5 mb-0.5 w-full justify-center'>
-                            <img src='Aai ji honey.jpg' className='w-18 rounded-full' />
-                            <h1 className='font-light-bold text-5xl'>Aai Ji Honey</h1>
-                        </div>
-                        <div>
-                            <div className=''><h1 className='font-bold pl-1'>Head office</h1><h1 className='ml-5'>426,Aai mata colony, Megakheda,Post-Pipli Ahiran,Teh-Kunwariya,Dist-Rajsamand,Rajasthan, PIN-313327,India.</h1></div>
-                            <h1 className='flex items-center justify-center font-bold'>(Raw honey ,Processed Honey , Multi-floral Honey,Herbal Honey,Edible Honey)</h1>
-                        </div>
-                    </div>
-                    <div className='mt-2 flex border-2 border-black bg-amber-200'>
-                        <div className='w-3/5 border-r-2 border-black pr-2'>
-                            <h1 className='font-bold'>Customer Details :</h1>
-                            <div className='flex'><h1 className='font-bold'>GSTIN: </h1><h1> 08AHFPC5892E1ZC</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Name : </h1><h1> {order.name}</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Mobile: </h1><h1> {order.mobile}</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Email: </h1><h1> {order.email}</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Address : </h1><h1> {order.address}</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Order id : </h1><h1> {order._id}</h1></div>
-                        </div>
-                        <div className='w-2/5 pl-2'>
-                            <div className='flex'><h1 className='font-bold'>Date:</h1><h1> {new Date(order.createdAt).toLocaleString()}</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Invoice No.:</h1><h1>{order.invoiceNumber}</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Supply state :</h1><h1> Rajasthan</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Supply mode : </h1><h1> Delivery</h1></div>
-                            <div className='flex'><h1 className='font-bold'>Ordered by :</h1><h1> MR. Bhanwar lal</h1></div>
-                        </div>
-                    </div>
-                    <div className='mt-3'>
-                        <table className='w-full'>
-                            <thead>
-                                <tr className=' border-black'>
-                                    <th className='border-2 border-black'>S.No</th>
-                                    <th className='border-2 border-black'>Description of Items</th>
-                                    <th className='border-2 border-black'>HSN code</th>
-                                    <th className='border-2 border-black'>Quantity</th>
-                                    <th className='border-2 border-black'>Price ₹</th>
-                                    <th className='border-2 border-black'>Total Price ₹</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr className='row-span-2'>
-                                    <td className='font-bold border-2 border-black row-span-2 pl-3'>1</td>
-                                    <td className='border-2 border-black pl-1 row-span-2'>{order.product?.name || 'N/A'}</td>
-                                    <td className='font-bold border-2 border-black row-span-2 pl-3'>0409</td>
-                                    <td className='border-2 border-black pl-1 row-span-2'>{order.quantity}</td>
-                                    <td className='border-2 border-black pl-1 row-span-2'>{order.product?.price || 'N/A'}</td>
-                                    <td className='border-2 border-black pl-1 row-span-2'>{order.product?.price * order.quantity}</td>
-                                </tr>
-                                <tr className='mt-1'>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td className='font-bold border-2 border-black pl-1'>Pt fee ₹</td>
-                                    <td className='border-2 border-black pl-1'>{(order.product?.price * order.quantity * 0.0236).toFixed(2)}</td>
-                                    <td className='border-2 border-black pl-1'>{(order.product?.price * order.quantity * 0.0236) + (order.product?.price * order.quantity)}</td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td className='font-bold border-2 border-black pl-1'>Delivery ₹</td>
-                                    <td className=' border-2 border-black pl-1'>{order.product?.price * order.quantity < 990 ? 100 : 0}</td>
-                                    <td className=' border-2 border-black pl-1'>{((order.product?.price * order.quantity < 990 ? 100 : 0) + ((order.product?.price * order.quantity * 0.0236) + (order.product?.price * order.quantity))).toFixed(2)}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className='flex'>
-                        <div>
-                            <h1 className='font-bold'>Terms & Condition :</h1>
-                            <ul>
-                                <li>E & O.E</li>
-                                <li>All disputes subjects to "UDAIPUR" Jurisdictions only.</li>
-                            </ul>
-                        </div>
-                        <div className='flex mt-3 ml-6 float-right'>
-                            <div>
-                                <img src='Signature.png' className='w-20 ml-8' />
-                            </div>
-                            <div className='mt-5  text-right'>
-                                <h1 className='font-bold'>For - Aai Ji Honey</h1>
-                                <h4>Authorized signature</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
-      </body>
-    </html>
+        const handlePrint = (order) => {
+            const printWindow = window.open('', '', 'height=800,width=1000');
+
+            printWindow.document.write(`
+  <html>
+    <head>
+      <title>Invoice</title>
+      <style>
+        * {
+          box-sizing: border-box;
+        }
+        .page {
+          font-family: Arial, sans-serif;
+          padding: 20px;
+        }
+        .center {
+          text-align: center;
+        }
+        .mt-2 {
+          margin-top: 10px;
+        }
+        .w-full {
+          width: 100%;
+        }
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        th, td {
+          padding: 8px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="page">
+        <div class="center">
+          <h1>जय श्री आई माता नमो नमः</h1>
+          <h2>Tax Invoice</h2>
+          <h3>Aai Ji Honey</h3>
+        </div>
+
+        <div class="mt-2">
+          <p><strong>Name:</strong> ${order.name}</p>
+          <p><strong>Mobile:</strong> ${order.mobile}</p>
+          <p><strong>Email:</strong> ${order.email}</p>
+          <p><strong>Address:</strong> ${order.address}</p>
+          <p><strong>Order ID:</strong> ${order._id}</p>
+          <p><strong>Date:</strong> ${new Date(order.createdAt).toLocaleString()}</p>
+          <p><strong>Invoice No:</strong> ${order.invoiceNumber}</p>
+        </div>
+
+        <div class="mt-2">
+          <table class="w-full">
+            <thead>
+              <tr>
+                <th>S.No</th>
+                <th>Item</th>
+                <th>HSN</th>
+                <th>Qty</th>
+                <th>Price ₹</th>
+                <th>Total ₹</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>${order.product?.name || ''}</td>
+                <td>0409</td>
+                <td>${order.quantity}</td>
+                <td>${order.product?.price || 0}</td>
+                <td>${order.product?.price * order.quantity}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="mt-2 center">
+          <p>Thanks for shopping with Aai Ji Honey 🍯</p>
+          <p>426, Aai Mata Colony, Rajasthan, India</p>
+        </div>
+      </div>
+    </body>
+  </html>
   `);
+
+            printWindow.document.close();
+            printWindow.focus();
+            printWindow.print();
+            printWindow.close();
+        };
+
 
         printWindow.document.close();
         printWindow.focus();
@@ -238,10 +197,11 @@ const OrderDashboard = () => {
             </div>
 
             {/* Printable Hidden Section */}
-            <div id={`print-section-${order._id}`} className="hidden printable bg-white p-6">
+            {/* <div id={`print-section-${order._id}`} className="hidden printable bg-white p-6"> */}
 
-                {/* bill format */}
-                {/* <div>
+
+            {/* bill format */}
+            {/* <div>
                     <div className='bg-green-300 border-2 border-black'>
                         <div className='mx-auto text-center mt-2'>
                             <h1>Jai Shri Aai Mata Namo Namah</h1>
@@ -343,55 +303,55 @@ const OrderDashboard = () => {
                         </div>
                     </div>
                 </div> */}
-            </div>
         </div>
+        </div >
     );
 
 
-    return (
-        <div className="m-2.5 p-4 max-w-full mx-auto w-full mt-[-15px] mb-[-6px] bg-gradient-to-br from-yellow-100 to-yellow-300 shadow-lg">
-            <h2 className="text-3xl font-bold mb-6 mt-5 text-center">Order Dashboard</h2>
+return (
+    <div className="m-2.5 p-4 max-w-full mx-auto w-full mt-[-15px] mb-[-6px] bg-gradient-to-br from-yellow-100 to-yellow-300 shadow-lg">
+        <h2 className="text-3xl font-bold mb-6 mt-5 text-center">Order Dashboard</h2>
 
-            <div className='routes  space-x-3 mb-3'>
-                <span className="bg-gradient-to-r from-amber-200 to-amber-500 hover:from-amber-400 hover:to-amber-200 text-amber-900 font-semibold px-4 py-2 rounded-full shadow-md hover:shadow-lg transition-all duration-300 ease-in-out">
-                    <Link to="/" className="no-underline">Home</Link>
-                </span>
+        <div className='routes  space-x-3 mb-3'>
+            <span className="bg-gradient-to-r from-amber-200 to-amber-500 hover:from-amber-400 hover:to-amber-200 text-amber-900 font-semibold px-4 py-2 rounded-full shadow-md hover:shadow-lg transition-all duration-300 ease-in-out">
+                <Link to="/" className="no-underline">Home</Link>
+            </span>
 
-                <span className="bg-gradient-to-r from-amber-200 to-amber-500 hover:from-amber-400 hover:to-amber-200 text-amber-900 font-semibold px-4 py-2 rounded-full shadow-md hover:shadow-lg transition-all duration-300 ease-in-out">
-                    <Link to="/add-product" className="no-underline">Dashboard</Link>
-                </span>
+            <span className="bg-gradient-to-r from-amber-200 to-amber-500 hover:from-amber-400 hover:to-amber-200 text-amber-900 font-semibold px-4 py-2 rounded-full shadow-md hover:shadow-lg transition-all duration-300 ease-in-out">
+                <Link to="/add-product" className="no-underline">Dashboard</Link>
+            </span>
 
-            </div>
-
-            {loading ? (
-                <p>Loading orders...</p>
-            ) : (
-                <>
-                    <div className="mb-8">
-                        <h3 className="text-2xl font-semibold mb-4 hover:shadow-2xl">🕒 Pending Orders</h3>
-                        {pendingOrders.length === 0 ? (
-                            <p>No pending orders.</p>
-                        ) : (
-                            <div className="space-y-4">
-                                {pendingOrders.map(order => renderOrderCard(order, true))}
-                            </div>
-                        )}
-                    </div>
-
-                    <div>
-                        <h3 className="text-2xl font-semibold mb-4 hover:shadow-2xl">✅ Completed Orders</h3>
-                        {completedOrders.length === 0 ? (
-                            <p>No completed orders yet.</p>
-                        ) : (
-                            <div className="space-y-4">
-                                {completedOrders.map(order => renderOrderCard(order))}
-                            </div>
-                        )}
-                    </div>
-                </>
-            )}
         </div>
-    );
+
+        {loading ? (
+            <p>Loading orders...</p>
+        ) : (
+            <>
+                <div className="mb-8">
+                    <h3 className="text-2xl font-semibold mb-4 hover:shadow-2xl">🕒 Pending Orders</h3>
+                    {pendingOrders.length === 0 ? (
+                        <p>No pending orders.</p>
+                    ) : (
+                        <div className="space-y-4">
+                            {pendingOrders.map(order => renderOrderCard(order, true))}
+                        </div>
+                    )}
+                </div>
+
+                <div>
+                    <h3 className="text-2xl font-semibold mb-4 hover:shadow-2xl">✅ Completed Orders</h3>
+                    {completedOrders.length === 0 ? (
+                        <p>No completed orders yet.</p>
+                    ) : (
+                        <div className="space-y-4">
+                            {completedOrders.map(order => renderOrderCard(order))}
+                        </div>
+                    )}
+                </div>
+            </>
+        )}
+    </div>
+);
 };
 
 export default OrderDashboard;
