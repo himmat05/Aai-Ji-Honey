f408fab1e64905ea45243a496436a6d2ae8f04f3
Himmat Sirwee <himmatsirwee05@gmail.com>
2025-08-16 21:10:25 +0530

    cors change

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const JWT_SECRET = 'KOe5FSJKJcq7NX3ejOOOiJisiAD98iTO'; // Use a secure secret in
 
 
 app.use('/uploads', express.static('uploads'));
-app.options("*", cors());
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
