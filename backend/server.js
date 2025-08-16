8c4901174dcf2c5fb10d6f1a3cb57f78b1bef02a
Himmat Sirwee <himmatsirwee05@gmail.com>
2025-08-16 15:25:16 +0530

    key fix0

--- backend/server.js
+++ backend/server.js
@@ -264,7 +264,7 @@ app.patch('/orders/:id', async (req, res) => {
 // Configure Cloudinary for image uploads
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
